fa13a6f4d7cdb5851ebe9157ea87a30480a437fa
Adam Juras <hi@adamjuras.com>
2017-03-28 19:24:29 +0200

    Add error modal

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -13,8 +13,8 @@ module.exports = {
       } else {
         return res.data.main.temp
       }
-    }, function (res) {
-      throw new Error(res.message)
+    }, function (err) {
+      throw new Error('Unable to fetch weather for that location.')
     })
   }
 }
