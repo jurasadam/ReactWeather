587449c6203e7c075968abe99e886b696ab0fe86
Adam Juras <hi@adamjuras.com>
2017-03-28 20:40:56 +0200

    Moar styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react')
 var About = (props) => {
   return (
     <div>
-      <h1 className='text-center'>About</h1>
+      <h1 className='text-center page-title'>About</h1>
       <p>Simple weather app built on React based on Open Weather Map based on&nbsp;
         <a target="_blank" href="https://www.udemy.com/the-complete-react-web-app-developer-course/">
           The complete React web app developer course
--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -16,7 +16,7 @@ var WeatherForm = React.createClass({
       <div>
         <form onSubmit={this.onFormSubmit}>
           <div>
-            <input type='text' ref='city' placeholder='Enter city name' />
+            <input type='search' ref='city' placeholder='Search weather by city' />
           </div>
           <div>
             <button className='button expanded hollow'>Get Weather</button>
