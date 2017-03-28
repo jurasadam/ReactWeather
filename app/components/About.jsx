555efb7f89f92c8517bc8791174dbdc6cbc8526f
Adam Juras <hi@adamjuras.com>
2017-03-28 18:26:54 +0200

    Styled about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,14 @@ var React = require('react')
 
 var About = (props) => {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h1 className='text-center'>About</h1>
+      <p>Simple weather app built on React based on Open Weather Map based on&nbsp;
+        <a href="https://www.udemy.com/the-complete-react-web-app-developer-course/">
+          The complete React web app developer course
+        </a>
+      </p>
+    </div>
   )
 }
 
