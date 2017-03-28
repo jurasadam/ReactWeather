69829d17cf3080989c911879602ed9164bff613b
Adam Juras <hi@adamjuras.com>
2017-03-28 18:27:44 +0200

    Added target blank to a link

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,7 +5,7 @@ var About = (props) => {
     <div>
       <h1 className='text-center'>About</h1>
       <p>Simple weather app built on React based on Open Weather Map based on&nbsp;
-        <a href="https://www.udemy.com/the-complete-react-web-app-developer-course/">
+        <a target="_blank" href="https://www.udemy.com/the-complete-react-web-app-developer-course/">
           The complete React web app developer course
         </a>
       </p>
